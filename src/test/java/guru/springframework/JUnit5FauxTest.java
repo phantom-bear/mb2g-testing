4cb946f1e5fce32b42f8139aa65dcd2e9a99df5c
phantom-bear <phantombear6650@gamil.com>
2024-07-14 11:12:37 +0800

    add combo test

---
+++ src/test/java/guru/springframework/JUnit5FauxTest.java
@@ -0,0 +1,10 @@
+package guru.springframework;
+
+import org.junit.Test;
+
+public class JUnit5FauxTest {
+    @Test
+    public void someTestforJUnit5(){
+        System.out.println("I Ran....");
+    }
+}
