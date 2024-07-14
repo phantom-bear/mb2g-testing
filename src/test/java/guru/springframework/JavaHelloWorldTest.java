7999a37996571d8ec9c791ab6259fa263018539a
phantom-bear <phantombear6650@gamil.com>
2024-07-14 10:45:10 +0800

    pojo test

---
+++ src/test/java/guru/springframework/JavaHelloWorldTest.java
@@ -0,0 +1,12 @@
+package guru.springframework;
+
+
+public class JavaHelloWorldTest {
+
+    public void testGetHello(){
+        JavaHelloWorld javaHelloWorld = new JavaHelloWorld();
+
+        assert ("Hello World".equals(javaHelloWorld.getHello()));
+    }
+
+}
