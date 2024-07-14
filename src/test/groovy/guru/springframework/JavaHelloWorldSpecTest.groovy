82cddfb3b99bdc5957344ebfce704c0bb310b148
phantom-bear <phantombear6650@gamil.com>
2024-07-14 11:36:15 +0800

    add spcok test

---
+++ src/test/groovy/guru/springframework/JavaHelloWorldSpecTest.groovy
@@ -0,0 +1,12 @@
+package guru.springframework
+
+import spock.lang.Specification
+
+class JavaHelloWorldSpecTest extends Specification {
+    def "GetHello"() {
+        when:
+        def foo = "123"
+        then:
+        true
+    }
+}
